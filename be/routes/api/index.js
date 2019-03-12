3bc3a1031d2f8ea909e14fcd348c6f7d1f5e64bd
joony <ksj800417@gmail.com>
2019-03-12 21:43:13 +0900

    RESTful 테이터 테스트를 완료

---
+++ be/routes/api/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var createError = require('http-errors');
+var router = express.Router();
+
+
+router.use('/test', require('./test'))
+router.use('/user', require('./user'))
+
+router.all('*', function(req, res, next) {
+  next(createError(404, '그런 api 없어요'));
+});
+
+module.exports = router;
---
+++ be/routes/api/user/index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var createError = require('http-errors');
+var router = express.Router();
+
+  const us = [{
+      name: '길동이',
+      age: 14
+    },
+    {
+      name: '이이이',
+      age: 24
+    }
+  ]
+/* GET home page. */
+router.get('/',  (req, res, next) => {
+  console.log(req.query)
+  console.log(req.body)
+  res.send({users: us })
+});
+
+router.post('/', (req, res, next) => {
+  console.log(req.query)
+  console.log(req.body)
+  res.send({success : true, msg: 'post ok'})
+})
+
+router.put('/', (req, res, next) => {
+    console.log(req.query)
+    console.log(req.body)
+  res.send({success : true, msg: 'put ok'})
+})
+
+router.delete('/', (req, res, next) => {
+    console.log(req.query)
+    console.log(req.body)
+  res.send({success : true, msg: 'del ok'})
+})
+
+router.all('*',  (req, res, next) => {
+  next(createError(404, '그런 api 없어요'));
+});
+
+module.exports = router;
